cceb642fcf115cc5a6f332d6df00f11c153368bb
Patrick <ruomungai@gmail.com>
2022-06-12 08:06:49 +0300

    commit tree 8f4b31511483aca63ed9be8e24df9be856804ab3

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution here
+
+const findMatching=(array, name)=>{
+    const nameMatch = array.filter(function(matching){
+        return matching.toUpperCase() === name.toUpperCase()
+    })
+    return nameMatch;
+}
+
+const fuzzyMatch=(array, firstLetter)=>{
+    return array.filter(function(getFirstLetter){
+        return getFirstLetter.startsWith(firstLetter) 
+    })
+}
+
+
+function matchName(array, name){
+    function matching(nameList){
+        return nameList.name === name;
+    };
+    return array.filter(matching);
+}
